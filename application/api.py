78ee612b3672fcbe678746120219510ef44c9792
Luke Marshall <lukasmarshall@gmail.com>
2018-11-22 16:56:41 +1100

    Slight refactor on file paths for the input json files - needed to use os.path.join to deal with the fact that windows and linux use different types of slashes. Also made it a relative (rather than absolute) path

--- application/api.py
+++ application/api.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-cwd = os.getcwd()
+# cwd = os.getcwd()
 
 
 
@@ -21,7 +21,7 @@ def base():
 
 @app.route('/LoadProfiles/Avg')
 def avgload():
-    with open('{}\Tariff-API\AllData.json'.format(cwd)) as data_file:
+    with open(os.path.join('application','AllData.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
 
@@ -31,7 +31,7 @@ def avgload():
 
 @app.route('/LoadProfiles/Demog/<lpnum>')
 def data(lpnum):
-    with open('{}\Tariff-API\AllData_Demog.json'.format(cwd)) as data_file:
+    with open(os.path.join('application','AllData_Demog.json')) as data_file:
         data_loaded = json.load(data_file)
         for i in range(len(data_loaded)):
             data_loaded[i] = {k: data_loaded[i][k] for k in data_loaded[i] if (k == lpnum or k == 'TS')}
@@ -44,7 +44,7 @@ def data(lpnum):
 
 @app.route('/Tariffs/AllTariffs')
 def Alltariffs():
-    with open('{}\Tariff-API\AllTariffs.json'.format(cwd)) as data_file:
+    with open(os.path.join('application','AllTariffs.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
 
