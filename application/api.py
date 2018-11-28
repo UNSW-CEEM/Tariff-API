af5b8970e431a6c7fcffd618fb64028f7568ac56
Navid Haghdadi <n.haghdadi@gmail.com>
2018-11-28 15:49:35 +1100

    added a function for retail tariff

--- application/api.py
+++ application/api.py
@@ -44,8 +44,15 @@ def data(lpnum):
 
 @app.route('/Tariffs/AllTariffs')
 def Alltariffs():
-    with open(os.path.join('application','AllTariffs.json')) as data_file:
+    with open(os.path.join('application', 'AllTariffs.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
+
+@app.route('/elec-tariffs/retail')
+def retail_tariff():
+    with open(os.path.join('application', 'AllTariffs.json')) as data_file:
+        data_loaded = json.load(data_file)
+        return jsonify(data_loaded)
+
 
 
