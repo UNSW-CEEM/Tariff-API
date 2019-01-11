3ab48bb2fee1d4f60efb5087d35307f7953a4971
Navid Haghdadi <n.haghdadi@gmail.com>
2019-01-11 22:15:21 +1100

    Added network tariff call

--- application/api.py
+++ application/api.py
@@ -54,6 +54,12 @@ def retail_tariff():
     with open(os.path.join('application', 'AllTariffs.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
+    
+@app.route('/elec-tariffs/network')
+def network_tariff():
+    with open(os.path.join('application', 'AllTariffs_Network.json')) as data_file:
+         data_loaded = json.load(data_file)
+         return jsonify(data_loaded)
 
 
 
