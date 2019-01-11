50a3ea3403a579080184159aa0cc412346e7d993
Navid Haghdadi <n.haghdadi@unsw.edu.au>
2019-01-11 22:25:57 +1100

    added call to api and added some tariffs and quality check

--- application/api.py
+++ application/api.py
@@ -55,5 +55,8 @@ def retail_tariff():
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
 
-
-
+@app.route('/elec-tariffs/network')
+def network_tariff():
+    with open(os.path.join('application', 'AllTariffs_Network.json')) as data_file:
+         data_loaded = json.load(data_file)
+         return jsonify(data_loaded)
