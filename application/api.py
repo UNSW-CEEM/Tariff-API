0bb63300373f2148c2284a459a6bca2e08a7498d
Navid Haghdadi <n.haghdadi@gmail.com>
2019-08-08 16:49:52 +1000

    testing

--- application/api.py
+++ application/api.py
@@ -49,45 +49,60 @@ def Alltariffs():
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
 
+# # This part is for previous versions of retail tariffs
+# @app.route('/elec-tariffs/retail-previous-version/<version>')
+# def retail_tariff(version):
+#     with open(os.path.join('application', 'AllTariffs_Retail_{}.json'.format(version))) as data_file:
+#         data_loaded = json.load(data_file)
+#         return jsonify(data_loaded)
+#
+# #  Track the versions and dates
+# @app.route('/elec-tariffs/retail-previous-version-list')
+# def retail_tariff(version):
+#     with open(os.path.join('application', 'AllTariffs_Retail_Version_Track.json')) as data_file:
+#         data_loaded = json.load(data_file)
+#         return jsonify(data_loaded)
+#
+# # Most up to date version
+# @app.route('/elec-tariffs/retail')
+# def retail_tariff():
+#     with open(os.path.join('application', 'AllTariffs_Retail.json')) as data_file:
+#         data_loaded = json.load(data_file)
+#         return jsonify(data_loaded)
+#
+#  # This part is for previous versions of network tariffs
+#
+# @app.route('/elec-tariffs/network-previous-versions/<version>')
+# def network_tariff(version):
+#     with open(os.path.join('application', 'AllTariffs_Network_{}.json'.format(version))) as data_file:
+#          data_loaded = json.load(data_file)
+#          return jsonify(data_loaded)
+#
+# #  Track the versions and dates
+# @app.route('/elec-tariffs/network-previous-versions-list')
+# def network_tariff():
+#     with open(os.path.join('application', 'AllTariffs_Network_Version_Track.json')) as data_file:
+#          data_loaded = json.load(data_file)
+#          return jsonify(data_loaded)
+#
+# # Most up to date version
+# @app.route('/elec-tariffs/network')
+# def network_tariff():
+#     with open(os.path.join('application', 'AllTariffs_Network.json')) as data_file:
+#          data_loaded = json.load(data_file)
+#          return jsonify(data_loaded)
+
+
 # This part is for previous versions of retail tariffs
-@app.route('/elec-tariffs/retail-previous-version/<version>')
-def retail_tariff(version):
+@app.route('/elec-tariffs/retail/<version>')
+def retail_tariff_v(version):
     with open(os.path.join('application', 'AllTariffs_Retail_{}.json'.format(version))) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
 
 #  Track the versions and dates
-@app.route('/elec-tariffs/retail-previous-version-list')
-def retail_tariff(version):
-    with open(os.path.join('application', 'AllTariffs_Retail_Version_Track.json')) as data_file:
-        data_loaded = json.load(data_file)
-        return jsonify(data_loaded)
-
-# Most up to date version
 @app.route('/elec-tariffs/retail')
 def retail_tariff():
-    with open(os.path.join('application', 'AllTariffs_Retail.json')) as data_file:
+    with open(os.path.join('application', 'AllTariffs_Retail_.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
-
- # This part is for previous versions of network tariffs
-
-@app.route('/elec-tariffs/network-previous-versions/<version>')
-def network_tariff(version):
-    with open(os.path.join('application', 'AllTariffs_Network_{}.json'.format(version))) as data_file:
-         data_loaded = json.load(data_file)
-         return jsonify(data_loaded)
-
-#  Track the versions and dates
-@app.route('/elec-tariffs/network-previous-versions-list')
-def network_tariff():
-    with open(os.path.join('application', 'AllTariffs_Network_Version_Track.json')) as data_file:
-         data_loaded = json.load(data_file)
-         return jsonify(data_loaded)
-
-# Most up to date version
-@app.route('/elec-tariffs/network')
-def network_tariff():
-    with open(os.path.join('application', 'AllTariffs_Network.json')) as data_file:
-         data_loaded = json.load(data_file)
-         return jsonify(data_loaded)
