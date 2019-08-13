d0681e34e152438a2fd846295e8abda8ad7d04f8
Navid Haghdadi <n.haghdadi@gmail.com>
2019-08-13 15:27:05 +1000

    Added weather data from NASA Power

--- application/api.py
+++ application/api.py
@@ -4,6 +4,7 @@ import io
 import json
 import pandas as pd
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
@@ -125,3 +126,14 @@ def network_tariff():
     with open(os.path.join('application', 'AllTariffs_Network.json')) as data_file:
         data_loaded = json.load(data_file)
         return jsonify(data_loaded)
+
+
+#  most recent version
+@app.route('/weather/<startdate>/<enddate>/<site>')
+def weather_data(startdate, enddate, site):
+    with sqlite3.connect('nasa_power.db') as con:
+        newdata = pd.read_sql_query(con=con,
+                                    sql='select TS, CDD, HDD from HDDCDD where TS > {} and TS < {} and Site == {}'.format(
+                                        startdate, enddate, site))
+        newdata2 = newdata.to_json(orient='records')
+        return jsonify(newdata2)
